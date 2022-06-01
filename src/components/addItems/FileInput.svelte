b2bde243c05481eb5e7b37aa06731d070ad2153d
Marie Pelletier <marie.pelletier@gmail.com>
2022-06-01 13:59:25 -0400

    skip duplicates

--- src/components/addItems/FileInput.svelte
+++ src/components/addItems/FileInput.svelte
@@ -25,6 +25,7 @@
   let fileName;
   let file;
   let atNotebook;
+  let skipDuplicates = false;
   $: atNotebook =
     $page.path && $page.path.startsWith("/notebooks/") ? true : false;
 
@@ -33,11 +34,12 @@
 
   let error = false;
 
+  $: console.log('skip?', skipDuplicates)
+
   async function uploadNotes() {
 
-
     if ($selectedSource || 
-    ($selectedNotebooks && $selectedNotebooks.length) || noteColour) {
+    ($selectedNotebooks && $selectedNotebooks.length) || noteColour || skipDuplicates) {
 
       let body = {};
       if ($selectedNotebooks && $selectedNotebooks.length) {
@@ -53,6 +55,10 @@
 
       if (noteColour) {
         body.tags = $tags.getIds([noteColour]);
+      }
+
+      if (skipDuplicates) {
+        body.skipDuplicates = skipDuplicates
       }
 
       await fetch("/api/notes", {
@@ -152,6 +158,10 @@
     }
   }
 
+  }
+
+  function toggleDuplicates() {
+    skipDuplicates = !skipDuplicates
   }
 </script>
 
@@ -288,6 +298,13 @@ Upload URL endpoint should take content type as a parameter and return a {public
   {#if original}
     <input type="hidden" name="uploadURL" value={original} />
   {/if}
+
+  Skip Duplicates <input 
+  type="checkbox"
+  value={skipDuplicates}
+  on:change={toggleDuplicates}
+  label="Skip Duplicates" />
+
   <label class="input" class:done class:failed class:working class:error>
     {#if file}{file.name}{:else}Upload a file{/if}
     <input
@@ -301,6 +318,7 @@ Upload URL endpoint should take content type as a parameter and return a {public
   </label>
 
   {#if notesImport}
+
   <Button click={uploadNotes} disabled={!file} light={true}>Upload Notes</Button>
   {/if}
   {#if error}
